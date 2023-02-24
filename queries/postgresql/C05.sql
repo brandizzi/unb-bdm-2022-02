40b9244fd636f8e3412c86290f324b598fa9b802
Adam Brandizzi <adam@brandizzi.com.br>
2023-02-24 09:20:35 -0300

    Re-ordering Querie for postgresql, and rerunning benchmark

--- queries/postgresql/C05.sql
+++ queries/postgresql/C05.sql
@@ -1,3 +1,3 @@
--- Contar todas viagens cuja estação de embarque é a mesma que a estação de devloução	
-select count(*) from "VIAGENS2".viagens viagem  inner join "VIAGENS2".estacoes estacaoretirada on  viagem."idEstacaoRetirada"  = estacaoretirada."idEstacao" inner join  "VIAGENS2".estacoes estacaodevolucao on viagem."idEstacaoDevolucao"  = estacaodevolucao."idEstacao" where viagem."idEstacaoRetirada" = viagem."idEstacaoDevolucao";
+-- Contar todas viagens cujo embarque foi na estação Catedral Metropolitana (id 7 ) e o Desembarque foi na estação Rodoviária (id 6)	
+select count(*) from "VIAGENS2".viagens viagem  inner join "VIAGENS2".estacoes estacaoretirada on  viagem."idEstacaoRetirada"  = estacaoretirada."idEstacao" inner join  "VIAGENS2".estacoes estacaodevolucao on viagem."idEstacaoDevolucao"  = estacaodevolucao."idEstacao" where viagem."idEstacaoRetirada" = 7 and viagem."idEstacaoDevolucao" = 6 ;
 
